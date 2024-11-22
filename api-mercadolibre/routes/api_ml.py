bca06f9d20ec99748f871b0d873d0885bdfaf2d7
Elmer <renehiguera15@gmail.com>
2024-11-21 19:53:07 -0700

    message

--- api-mercadolibre/routes/api_ml.py
+++ api-mercadolibre/routes/api_ml.py
@@ -40,7 +40,7 @@ async def get_producto():
 async def check_connection():
     
     petition = ApiUtility.check_api_connection()
-    return 'docker run'
+    return 'docker ruuuun'
 
 @router.get("/precios")
 async def comparar_precios():
