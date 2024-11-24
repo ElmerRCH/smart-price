928fe311ab5368060ca88f7b50a896611b0576e3
Elmer Camacho <117885631+ElmerRCH@users.noreply.github.com>
2024-11-23 18:30:39 -0600

    Update api_ml.py

--- api-mercadolibre/routes/api_ml.py
+++ api-mercadolibre/routes/api_ml.py
@@ -40,7 +40,7 @@ async def get_producto():
 async def check_connection():
     
     petition = ApiUtility.check_api_connection()
-    return 'docker activo2222'
+    return 'docker activo'
 
 @router.get("/precios")
 async def comparar_precios():
